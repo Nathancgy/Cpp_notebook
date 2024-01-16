719b8d1ba29fbc41bb4e7986e49faf86072ac464
Nathan Chen <guangyu.chen40730-biph@basischina.com>
2024-01-16 23:51:27 +0800

    updated default template

---
+++ default.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+#include <climits>
+#include <cfloat>
+#include <ctime>
+#include <cassert>
+
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define sz(x) (int)x.size()
+#define mo 1000000007
+#define ep() empty()
+#define pii pair<ll,ll>
+#define rep(a,b) for(int a=0;a<b;a++)
+#define forn(a,b,c) for(int a=b;a<=c;a++)
+#define vll vector<long long>
+
+// ------------------------------------------------------------
+
+void solve(){
+    
+    return;
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t=1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
